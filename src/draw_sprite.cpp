f9d16e9d0498379930ab392e646cce3519c29d38
Pablo Arias <pabloariasal@gmail.com>
2021-11-28 01:17:50 +0100

    refactor rendering code

--- src/draw_sprite.cpp
+++ src/draw_sprite.cpp
@@ -4,46 +4,38 @@
 
 namespace {
 
-std::array<bool, 8> toArray(uint8_t row) {
-  std::array<bool, 8> sprite{};
-  for (int i = 0; i < 8; ++i) {
-    if ((row & 0x01u) == 1) {
-      sprite[7 - i] = true;
-    }
-    row >>= 1;
-  }
-  /* std::reverse(sprite.begin(), sprite.end()); */
-  return sprite;
+bool isPixelSet(int index, uint8_t row) {
+  row >>= 7 - index;
+  return (row & 0x01u) == 1;
 }
 
+bool invertColor(Color& color) {
+  if (color == Color::BLACK) {
+    color = Color::WHITE;
+    return true;
+  }
+  color = Color::BLACK;
+  return false;
+}
 }  // namespace
 
 bool drawSprite(int row, int col, const std::vector<uint8_t>& sprite,
                 PixelBuffer<Color>& display) {
   auto flipped = false;
 
-  auto y = row % display.height();
-  for (const auto& sprite_row : sprite) {
-    auto x = col % display.width();
-    auto sprite_row_array = toArray(sprite_row);
-    for (auto sprite_pixel : sprite_row_array) {
-      if (sprite_pixel) {
-        if (display.get(y, x) == Color::BLACK) {
-          display.get(y, x) = Color::WHITE;
+  // wrap coordinates
+  row = row % display.height();
+  col = col % display.width();
+
+  for (int sr = 0, x = row; sr < (int)sprite.size() && x < display.height();
+       sr++, ++x) {
+    for (int sc = 0, y = col; sc < 8 && y < display.width(); sc++, ++y) {
+      if (isPixelSet(sc, sprite.at(sr))) {
+        if (invertColor(display.get(x, y))) {
           flipped = true;
-        } else {
-          display.get(y, x) = Color::BLACK;
         }
       }
-      if (x >= display.width()) {
-        break;
-      }
-      ++x;
     }
-    if (y >= display.height()) {
-      break;
-    }
-    ++y;
   }
 
   return flipped;
